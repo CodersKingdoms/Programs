7857e8aef3694a5546fef8e3205ac692c392917c
CodersKingdoms <104496124+CodersKingdoms@users.noreply.github.com>
2022-10-17 12:56:05 +0530

    Area of circle using C

---
+++ area of circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    float r,A;
+    printf("enter the radius of circle ");
+    scanf("%f",&r);
+    A=3.14*r*r;
+    printf("\narea of circle=%f",A);
+    getch();
+}
