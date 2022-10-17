ae67df04558ec010432646781f6e227f9988dacd
CodersKingdoms <104496124+CodersKingdoms@users.noreply.github.com>
2022-10-17 12:55:31 +0530

    Without using remainder operator

---
+++ even or odd without %.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+int a;
+printf("enter any number");
+scanf("%d",&a);
+   {
+      if(a/2*2==a)
+         printf("number is even");
+   else
+         printf("number is odd");
+   }
+
+}
