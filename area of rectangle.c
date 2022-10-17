bea28f2caa6016d06ea6dacffde30cc2b1dbac71
CodersKingdoms <104496124+CodersKingdoms@users.noreply.github.com>
2022-10-17 12:54:48 +0530

    Areaofrect

---
+++ area of rectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    int l,b,area;
+    printf("enter the length and breadth of rectangle");
+    scanf("%d%d",&l,&b);
+    area = l*b;
+    printf("\n area of rectangle is %d",area);
+
+}
